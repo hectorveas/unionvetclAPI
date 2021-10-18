28a456fe55c385bbc6da51a941d628372a7d8ad6
Hectorveas <hveas10@gmail.com>
2021-10-17 22:08:22 -0300

    feat(appointment/schemas): populate with users and observations

--- src/appointment/schemas/appointment.schema.ts
+++ src/appointment/schemas/appointment.schema.ts
@@ -2,11 +2,14 @@ import { Schema } from 'mongoose';
 
 export const AppointmentSchema = new Schema(
   {
-    idUser: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    idUser: { type: Schema.Types.ObjectId, ref: 'User', required: true, autopopulate: { maxDepth: 2 } },
+    userName: { type: String, required: false },	
+    userLastName: { type: String, required: false },	
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
-    state: { type: String, default: 'pending' },
-    observation: { type: Schema.Types.ObjectId, ref: 'Observation' },
+    state: { type: Number, default: 0 },
+    observation: [{ type: Schema.Types.ObjectId, ref: 'Observation', required: false, autopopulate: { maxDepth: 2 } }],
   },
   { timestamps: true },
 );
+AppointmentSchema.plugin(require('mongoose-autopopulate'));
