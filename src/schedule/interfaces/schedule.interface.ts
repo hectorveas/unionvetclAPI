b67ceb1985fb00f23ae2883324d56fa5e7248bfd
Hectorveas <hveas10@gmail.com>
2021-10-17 22:05:46 -0300

    refactor(schedule/interfaces):  year deleted

--- src/schedule/interfaces/schedule.interface.ts
+++ src/schedule/interfaces/schedule.interface.ts
@@ -5,7 +5,6 @@ export interface Schedule extends Document {
   readonly startDate: Date;
   readonly endDate: Date;
   readonly month: string;
-  readonly year: string;
   readonly day: string;
   readonly maxAppointment: number;
   readonly appointments: [Appointment];
